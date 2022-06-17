7d9d92c27f042021744f0c43c15865816413826a
Alexandru Ticau <alex.ticau2102@gmail.com>
2022-06-17 11:22:03 +0300

    Add hard/soft skills sections , fix display bugs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <div name="about" className="w-full h-[150vh] bg-[#0A1929] text-gray-300">
+    <div name="about" className="w-full h-[180vh] bg-[#0A1929] text-gray-300">
       <div className="flex flex-col justify-center items-center w-full h-screen">
         <div className="max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8">
           <div className="sm:text-right pb-8 pl-4">
@@ -46,12 +46,19 @@ const About = () => {
             <div>
             <p className="text-2xl border-b-2 border-[#EE550E]">Soft Skills :</p>
               <ul>
-                <li>Javascript</li>
-                <li>React</li>
-                <li>Express</li>
-                <li>MongoDB</li>
-                <li>npm</li>
-                <li>git</li>
+                <li className="py-2"><img className="w-[30px] inline mr-2" src="./src/assets/stress.png" />Stress-resistant</li>
+                <li className="py-2"><img className="w-[30px] inline mr-2" src="./src/assets/team.png" />Team player</li>
+                <li className="py-2"><img className="w-[30px] inline mr-2" src="./src/assets/open-mind.png" />Open minded</li>
+                <li className="py-2"><img className="w-[30px] inline mr-2" src="./src/assets/time.png" />Time management skills</li>
+                <li className="py-2"><img className="w-[30px] inline mr-2" src="./src/assets/communication.png" />Communication skills</li>
+                <li className="py-2"><img className="w-[30px] inline mr-2" src="./src/assets/discipline.png" />Disciplined</li>
+                <li className="py-2"><img className="w-[30px] inline mr-2" src="./src/assets/problem.png" />Problem Solving skills</li>
+                <div className="flex mx-auto h-[60px] justify-center mt-10">
+                  <a href="src/assets/CV_Ticau_Alexandru_ro.pdf"><div className=" mx-4 text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-[#EE550E] hover:border-[#EE550E]"> CV RO</div>
+                   </a>
+                   <a href="src/assets/CV_Ticau_Alexandru_en.pdf"><div className=" mx-4 text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-[#EE550E] hover:border-[#EE550E]"> CV EN</div>
+                   </a>
+                </div>
               </ul>
             </div>
         </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import {HiArrowNarrowRight} from "react-icons/hi";
 const Home = () => {
   return (
     <div name='home' className='bg-[#0A1929] h-[180vh] w-full lg:flex lg:h-screen'>
-        <div className='lg:ml-[30px] max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-1/2 lg:h-full'>
+        <div className='lg:ml-[30px] max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-[40%] lg:h-full'>
             <p className='text-2xl text-[#EE550E]'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Alexandru Ticau</h1>
             <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a junior front-end developer</h2>
